dd17a142983c869da22e101df6c10a49fc7969ed
Bill Gleim <bgleim@gmail.com>
2019-05-21 14:21:21 -0700

    Update run_eve.sh
    add npm installation

--- aws/run_eve.sh
+++ aws/run_eve.sh
@@ -2,7 +2,7 @@
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get -y install curl git vim build-essential
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install -y nodejs npm
 sudo npm install -g express
 
 # install Truffle
